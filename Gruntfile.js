951851ca643c261d48d1c31703ede180078207da
Timo Tijhof <krinklemail@gmail.com>
2013-10-10 03:39:12 +0200

    grunt: Add compare_size

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,27 @@
 /*jshint node:true */
 module.exports = function (grunt) {
+	grunt.loadNpmTasks('grunt-compare-size');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		compare_size: {
+			files: [
+				'src/*.{js,css}'
+			],
+			options: {
+				// Location of stored size data
+				cache: '.sizecache.json',
+
+				// Compressor label-function pairs
+				compress: {
+					gz: function (fileContents) {
+						return require('gzip-js').zip(fileContents, {}).length;
+					}
+				}
+			}
+		},
 		jshint: {
 			all: ['*.js', 'src/*.js']
 		},
